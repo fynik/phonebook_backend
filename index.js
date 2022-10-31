ddb68309e458bbd05d2cdfcb7803579bcd722386
FynIk <ash.glc@gmail.com>
2022-10-31 17:49:51 +0600

    step2-6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ express = require("express");
 const app = express();
 app.use(express.json());
 
-const persons = [
+let persons = [
   { 
     "id": 1,
     "name": "Arto Hellas", 
@@ -28,6 +28,65 @@ const persons = [
 
 app.get("/api/persons", (req, res) => {
   res.json(persons);
+})
+
+app.get("/api/info", (req, res) => {
+  res.send(
+    `<p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>`
+  )
+})
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+})
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  persons = persons.filter(p => p.id !== id);
+  res.status(204).end();
+})
+
+const generateId = () => {
+  const rangeUpperLimit = 10000;
+  const currentIds = persons.map(p => p.id);
+
+  let newId = Math.floor(1 + Math.random() * rangeUpperLimit);
+
+  while (currentIds.includes(newId)) {
+    newId = Math.floor(1 + Math.random() * rangeUpperLimit);
+  }
+  
+  return newId;
+}
+
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({ error: "content missing" });
+  }
+  if (persons.map(p => p.name).includes(body.name)) {
+    return res.status(400).json({ error: "name must be unique" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId()
+  }
+
+  persons = persons.concat(person);
+  res.json(person);
 })
 
 const PORT = 3001;
