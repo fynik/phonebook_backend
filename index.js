9cdb7e3fcbed0969cbeeaaee54b2ce6d3c8cca70
FynIk <ash.glc@gmail.com>
2022-11-01 15:01:40 +0600

    removed vercel

--- index.js
+++ index.js
@@ -100,5 +100,3 @@ app.post("/api/persons", (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-module.exports = app;
